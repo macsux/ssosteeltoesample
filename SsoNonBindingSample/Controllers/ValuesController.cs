86bdda1e6642249b5ddffffd13af8f1e0a94842d
Andrew Stakhov <astakhov@pivotal.io>
2018-10-16 15:17:11 -0400

    Code

--- SsoNonBindingSample/Controllers/ValuesController.cs
+++ SsoNonBindingSample/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SsoNonBindingSample.Controllers
@@ -12,6 +13,7 @@ namespace SsoNonBindingSample.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Authorize(policy: "read")]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
--- SsoNonBindingSample/Startup.cs
+++ SsoNonBindingSample/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.Security.Authentication.CloudFoundry;
 
 namespace SsoNonBindingSample
 {
@@ -25,12 +28,20 @@ namespace SsoNonBindingSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddCloudFoundryJwtBearer(Configuration);
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("read", policy => policy.RequireClaim("scope", "todo.read"));
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
